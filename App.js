eb351727101118f683d53334970a23307a437ad8
Ditookkk <muhammad.119140170@student.itera.ac.id>
2022-03-27 04:19:56 +0700

    update

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
-import React, {Component} from "react";
-import {View} from 'react-native'
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {  };
-    
-  }
-  render() {
-    return (
-      <View style={{flex :1,backgroundColor: '#f2f2f2'}}>
-      
-      </View>
-    );
-  }
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
