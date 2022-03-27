4d82b2f04b3f8187ce2d21545620701dc7354179
Ditookkk <muhammad.119140170@student.itera.ac.id>
2022-03-27 10:38:16 +0700

    Update lagi

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -1,184 +1,78 @@
 
-export const MASKAPAI = {
-    GRD:{
-        maskapai_id: "GRD",
-        maskapai_nama: "Garuda Airline",
-        maskapai_logo: "../images/garuda.jpg", 
+export const PELABUHAN = {
+    BKH:{
+        pelabuhan_id: "BKH",
+        pelabuhan_nama: "Bakauheni", 
     },
-    SJW:{
-        maskapai_id: "SJW",
-        maskapai_nama: "Seriwijaya Air",
-        maskapai_logo: "../images/Sriwijaya_air.png", 
+    MRK:{
+        pelabuhan_id: "MRK",
+        pelabuhan_nama: "Merak", 
     },
-    LIO:{
-        maskapai_id: "LIO",
-        maskapai_nama: "Lion Air",
-        maskapai_logo: "../images/lion_air.png", 
+    TPR:{
+        pelabuhan_id: "TPR",
+        pelabuhan_nama: "Tanjung Priok", 
     },
-    PLA:{
-        maskapai_id: "PLA",
-        maskapai_nama: "Pelita Air",
-        maskapai_logo: "../images/pelita_air.png",
+    TBY:{
+        pelabuhan_id: "TBY",
+        pelabuhan_nama: "Teluk Bayur", 
     },
-    NSA:{
-        maskapai_id: "NSA",
-        maskapai_nama: "Nusantara Air",
-        maskapai_logo: "../images/nusantara.png",
-    },
-    CTL:{
-        maskapai_id: "CTL",
-        maskapai_nama: "Citilink",
-        maskapai_logo: "../images/citilink.png",
+    BPN:{
+        pelabuhan_id: "BPN",
+        pelabuhan_nama: "BalikPapan", 
     },
 };
 
-export const BANDARA = {
-    SKH:{
-        bandara_kode: "SKH",
-        bandara_nama: "Soekarno Hatta",
+export const LAYANAN = {
+    ECO:{
+        layanan_kode: "ECO",
+        layanan_nama: "Economy",
     },
-    RDN:{
-        bandara_kode: "RDN",
-        bandara_nama: "Raden Intan",
+    EXC:{
+        bandara_kode: "EXC",
+        bandara_nama: "Executive",
     },
-    NGR:{
-        bandara_kode: "NGR",
-        bandara_nama: "Ngurah Rai",
-    },
-    JND:{
-        bandara_kode: "JND",
-        bandara_nama: "Juanda",
-    },
-    NNK:{
-        bandara_kode: "NNK",
-        bandara_nama: "Nunukan",
-    },
-    KLN:{
-        bandara_kode: "KLN",
-        bandara_nama: "Kualanamu",
+    BUS:{
+        bandara_kode: "BUS",
+        bandara_nama: "Bussiness",
     },
 };
 
 export const JADWAL = [
     {
         jadwal_id: "1",
-        bandara_kode_keberangkatan: "SKH",
-        bandara_kode_tujuan: "RDN",
-        maskapai_id: "GRD"
+        pelabuhan_kode_keberangkatan: "BKH",
+        pelabuhan_kode_tujuan: "MRK",
+        layanan_kode: "ECO",
+        harga : "50.000"
     },
     {
         jadwal_id: "2",
-        bandara_kode_keberangkatan: "SKH",
-        bandara_kode_tujuan: "NGR",
-        maskapai_id: "LIO"
+        pelabuhan_kode_keberangkatan: "TPR",
+        pelabuhan_kode_tujuan: "MRK",
+        layanan_kode: "BUS",
+        harga : "100.000"
     },
     {
         jadwal_id: "3",
-        bandara_kode_keberangkatan: "RDN",
-        bandara_kode_tujuan: "NGR",
-        maskapai_id: "SJW"
+        pelabuhan_kode_keberangkatan: "TBY",
+        pelabuhan_kode_tujuan: "TPR",
+        layanan_kode: "EXC",
+        harga : "450.000"
     },
     {
         jadwal_id: "4",
-        bandara_kode_keberangkatan: "NGR",
-        bandara_kode_tujuan: "SKH",
-        maskapai_id: "GRD"
+        pelabuhan_kode_keberangkatan: "BKH",
+        pelabuhan_kode_tujuan: "BPN",
+        layanan_kode: "ECO",
+        harga : "250.000"
     },
     {
         jadwal_id: "5",
-        bandara_kode_keberangkatan: "JND",
-        bandara_kode_tujuan: "NNK",
-        maskapai_id: "CTL"
+        pelabuhan_kode_keberangkatan: "BPN",
+        pelabuhan_kode_tujuan: "MRK",
+        layanan_kode: "BUS",
+        harga : "450.000"
     },
-    {
-        jadwal_id: "6",
-        bandara_kode_keberangkatan: "JND",
-        bandara_kode_tujuan: "KLN",
-        maskapai_id: "NSA"
-    },
-    {
-        jadwal_id: "7",
-        bandara_kode_keberangkatan: "NNK",
-        bandara_kode_tujuan: "KLN",
-        maskapai_id: "PLA"
-    },
-    {
-        jadwal_id: "8",
-        bandara_kode_keberangkatan: "NGR",
-        bandara_kode_tujuan: "NNK",
-        maskapai_id: "GRD"
-    },
-    {
-        jadwal_id: "9",
-        bandara_kode_keberangkatan: "SKH",
-        bandara_kode_tujuan: "KLN",
-        maskapai_id: "PLA"
-    },
-    {
-        jadwal_id: "10",
-        bandara_kode_keberangkatan: "RDN",
-        bandara_kode_tujuan: "NNK",
-        maskapai_id: "SJW"
-    },
-    {
-        jadwal_id: "11",
-        bandara_kode_keberangkatan: "SKH",
-        bandara_kode_tujuan: "NNK",
-        maskapai_id: "PLA"
-    },
-    {
-        jadwal_id: "12",
-        bandara_kode_keberangkatan: "SKH",
-        bandara_kode_tujuan: "RDN",
-        maskapai_id: "PLA"
-    },
-    {
-        jadwal_id: "13",
-        bandara_kode_keberangkatan: "SKH",
-        bandara_kode_tujuan: "RDN",
-        maskapai_id: "CTL"
-    },
-    {
-        jadwal_id: "14",
-        bandara_kode_keberangkatan: "NNK",
-        bandara_kode_tujuan: "SKH",
-        maskapai_id: "NSA"
-    },
-    {
-        jadwal_id: "15",
-        bandara_kode_keberangkatan: "KLN",
-        bandara_kode_tujuan: "SKH",
-        maskapai_id: "GRD"
-    },
-    {
-        jadwal_id: "16",
-        bandara_kode_keberangkatan: "KLN",
-        bandara_kode_tujuan: "SKH",
-        maskapai_id: "GRD"
-    },
-    {
-        jadwal_id: "17",
-        bandara_kode_keberangkatan: "KLN",
-        bandara_kode_tujuan: "SKH",
-        maskapai_id: "CTL"
-    },
-    {
-        jadwal_id: "18",
-        bandara_kode_keberangkatan: "KLN",
-        bandara_kode_tujuan: "SKH",
-        maskapai_id: "PLA"
-    },
-    {
-        jadwal_id: "19",
-        bandara_kode_keberangkatan: "KLN",
-        bandara_kode_tujuan: "SKH",
-        maskapai_id: "NSA"
-    },
-    {
-        jadwal_id: "20",
-        bandara_kode_keberangkatan: "KLN",
-        bandara_kode_tujuan: "SKH",
-        maskapai_id: "SJW"
-    },
+   
 
 ];
